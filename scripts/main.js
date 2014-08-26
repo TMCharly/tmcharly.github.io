261b507477880af9b760ab6c4b34ea074a2436ae
unknown <cedced19@gmail.com>
2014-08-26 09:32:51 +0200

    Fix a bug.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-var show = $('.show'),
-hover = $('.hover');
+var show = $('.show');
+var hover = $('.hover');
 
 show.hover(
   function() {
@@ -11,13 +11,10 @@ show.hover(
 
 hover.click(function() {
     show.remove();
-    var div = $('<div>').addClass('showed');
-    $('<img>').addClass('other').attr('src', 'http://image.noelshack.com/fichiers/2014/22/1401358226-wallpaper-vixen-1.jpg').appendTo(div);
-    $('<img>').addClass('other').attr('src', 'http://image.noelshack.com/fichiers/2014/22/1401358219-wallpaper-excision-1.jpg').appendTo(div);
-    $('<img>').addClass('other').attr('src', 'http://image.noelshack.com/fichiers/2014/22/1401358322-wpm.jpg').appendTo(div);
-    $('<img>').addClass('other').attr('src', 'http://image.noelshack.com/fichiers/2014/22/1401358248-f1-racing.jpg').appendTo(div);
-    $('<img>').addClass('other').attr('src', 'http://image.noelshack.com/fichiers/2014/22/1401358313-6-driver.jpg').appendTo(div);
-    $('<img>').addClass('other').attr('src', 'http://image.noelshack.com/fichiers/2014/22/1401358319-4.jpg').appendTo(div);
-    div.appendTo('.pictures');
-
+    var div = $('<div>').addClass('showed').appendTo('.pictures');
+    $.getJSON('/images.json', function (images) {
+          for (var image in images){
+            $('<img>').attr('src', images[image]).appendTo('.showed');
+          }
+    });
 });
